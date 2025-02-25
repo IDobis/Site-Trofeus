1ea760dc51380833351519038bdca8295cebba2d
IDobis <dobislothbrok@gmail.com>
2025-02-25 14:39:07 -0300

    Tema de Fundo.
    Correção ao carregar o tema escuro, não está mais dando uma piscada de tela branca.

--- script.js
+++ script.js
@@ -124,9 +124,9 @@ addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector(".toggle-button");
 
   function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
+    document.documentElement.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
+    if (document.documentElement.classList.contains("dark-mode")) {
       button.textContent = "Modo Claro";
       localStorage.setItem("theme", "dark");
     } else {
@@ -135,13 +135,11 @@ addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Salvar modo de Tela
+  // Trocar o nome do botão conforme o tema
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
-    document.body.classList.add("dark-mode");
     button.textContent = "Modo Claro";
   } else {
-    document.body.classList.remove("dark-mode");
     button.textContent = "Modo Escuro";
   }
 
